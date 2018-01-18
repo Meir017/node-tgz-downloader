f70042cfe1eaab427120454ca9afba6d45f88d1d
Meir Blachman <meirblachman@protonmail.com>
2018-01-18 19:01:36 +0200

    changed package.json

--- bin/index.js
+++ bin/index.js
@@ -1,6 +1,6 @@
 const [, , packageLockPath] = process.argv;
 
-const downloader = require("./downloader");
+const downloader = require("../lib/downloader");
 
 if (packageLockPath) {
   downloader.downloadFromPackageLock(packageLockPath);
